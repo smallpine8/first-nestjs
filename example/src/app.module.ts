fe9d75d80d4be9688df64bf4de6262e681ffb2f6
Komatsu Nobuyuki <abr0811@icloud.com>
2020-07-28 00:41:53 +0900

    TypeormでConnection

--- example/src/app.module.ts
+++ example/src/app.module.ts
@@ -1,17 +1,28 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatModule } from './cat/cat.module';
 
 import { LoggerMiddleware } from './middlewares/logger.middleware';
-
+const config = dotenv.parse(fs.readFileSync('./.env'));
+const entities = { entities: [] }
+const mergedConfig = Object.assign({}, config, entities);
 @Module({
-  imports: [CatModule],
+  imports: [
+    TypeOrmModule.forRoot(mergedConfig),
+    CatModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
+  constructor(private connection: Connection) {}
+  configure(consumer: MiddlewareConsumer)
+  {
     consumer
     .apply(LoggerMiddleware)
     .forRoutes('cat');
